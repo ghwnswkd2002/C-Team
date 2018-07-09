2ee62b40cf523498c0f762489b6d37956e0b957e
DESKTOP-8UMPF1A\BIT <saewho2@naver.com>
2018-07-10 00:15:37 +0900

    덜한거
    이거전거다

--- HelloWorld/src/main/java/com/owo/HelloWorld/Buffer/CodeBuffer_Impl.java
+++ HelloWorld/src/main/java/com/owo/HelloWorld/Buffer/CodeBuffer_Impl.java
@@ -41,12 +41,12 @@ public class CodeBuffer_Impl implements CodeBuffer{
 		String[] dev = line.split(" ");
 		param = new ParamBean();
 
-		System.out.println(line +"여긴 변수부분 찍히냐");
-		for(int i = 0; i<dev.length; i++) {
-			System.out.println("dev"+i+"="+dev[i]);
-		}
+//		System.out.println(line +"여긴 변수부분 찍히냐");
+//		for(int i = 0; i<dev.length; i++) {
+//			System.out.println("dev"+i+"="+dev[i]);
+//		}
 		
-		for(ParameterType parameterType : ParameterType.values()) {
+		for(ParameterType parameterType : ParameterType.values()) {//일반적으로안띄우고 선언한경우
 			System.out.println("for문 들어옴");
 			if(dev[0].toUpperCase().equals(parameterType.toString())&&!line.contains("=")) {
 				System.out.println("if문");
@@ -56,9 +56,10 @@ public class CodeBuffer_Impl implements CodeBuffer{
 				//toUpperCase() 이거는 소문자를 대문자로 바꾸는 함수
 				System.out.println(dev[0]+"이거 넣을게 초기화 안해도대는거야 ");
 				param.setParamType(dev[0]);
-				param.setParamName(dev[1].substring(0,dev[1].length()-1));
+				param.setParamName(dev[1].replaceAll(";",""));
 				
 			} else if(dev[0].toUpperCase().equals(parameterType.toString())&&line.contains("=")) {
+				//초기화 해서 선언한 경우
 				System.out.println("else if문");
 				//enum 안에 있는 String과 split한 배열 첫번째랑 같은게 있는지 확인
 				//근데 초깃값 설정 되어 있음
@@ -73,10 +74,20 @@ public class CodeBuffer_Impl implements CodeBuffer{
 				param.setParamName(temp[0]);
 				System.out.println("temp[0]에 넣음");
 				param.setParamLine(String.valueOf(lineNumber));
-				param.setParamValue(temp[1].substring(0,temp[1].length()-1));
-				
+				param.setParamValue(temp[1].replaceAll(";",""));
 				System.out.println("샷");
 			}
+			//초기화 할때 띄워서 선언된 경우
+			else if(dev[0].toUpperCase().equals(parameterType.toString())&&line.contains("=")&&dev.length>2) {
+				
+			}
+			//초기화 한 변수가 char에 문자열 담았을경우
+			else if(dev[0].equals("char")) {
+				
+			}
+			//포인터 인경우
+			
+			
 		}
 		return param;
 	}
