cfae72d50632d26184f46fb92c0ed44cb6d34cc0
Ryeom <38072339+Ryeom@users.noreply.github.com>
2018-07-09 13:21:33 +0900

    lll

--- HelloWorld/src/main/java/com/owo/HelloWorld/Buffer/CodeBuffer_Impl.java
+++ HelloWorld/src/main/java/com/owo/HelloWorld/Buffer/CodeBuffer_Impl.java
@@ -1,7 +1,10 @@
 package com.owo.HelloWorld.Buffer;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Repository;
 
+import com.owo.HelloWorld.Buffer.Bean.MethodBean;
 import com.owo.HelloWorld.Buffer.Bean.ParamBean;
 import com.owo.HelloWorld.Buffer.Enum.ParameterType;
 @Repository
@@ -14,53 +17,88 @@ public class CodeBuffer_Impl implements CodeBuffer{
 	 * 여기서는 라인을 하나하나 읽는 경우의 수를 생각해서 함수 만들어 줘야함
 	 * 	1. 첫 글자가 변수형태일 때
 	 *		- 변수 선언문		- 함수 선언
+	 *
 	 *	2. 제어문 경우
 	 *		- 반복문			- 제어문
+	 *
 	 *	3. 내장함수
 	 *		- printf/scanf	- 기타
-	 *	4. 기타(좀더 생각 해 볼것);	
+	 *
+	 *	4. 기타(좀더 생각 해 볼것);
+	 *
 	 */
-	
-	
-	ParameterType parameterType;//enum쓸꼬얌
-	ParamBean param;
 
-	
-	
-	public ParamBean case_VariableDeclaration(String line) {
-		
+
+	//ParameterType parameterType;//enum쓸꼬얌
+	ParamBean param;
+	MethodBean method;
+
+	@Override		//변수 선언부분 만났을 경우
+	public ParamBean case_VariableDeclaration(int lineNumber,String line) {
+
 		//String line ="char name;"
 		System.out.println("케이스 변수선언 들어옴");
 		String[] dev = line.split(" ");
 		param = new ParamBean();
-		
+
 		System.out.println(line +"여긴 변수부분 찍히냐");
-		
-		for(int i=0;i<dev.length;i++) {
-			System.out.println(dev[i]);
+		for(int i = 0; i<dev.length; i++) {
+			System.out.println("dev"+i+"="+dev[i]);
 		}
 		
-		if(parameterType.values().equals(dev[0])&&!line.contains("=")) {
-			//enum 안에 있는 Strind과 split한 배열 첫번째랑 같은게 있는지 확인
-			//근데 초깃값 설정안되어 있는 경우
-			//EX. int a;
-			
-			param.setParamType(dev[0]);
-			param.setParamName(dev[1].substring(0,dev[1].length()-1));
-		} else if(parameterType.values().equals(dev[0])&&line.contains("=")) {
-			//enum 안에 있는 String과 split한 배열 첫번째랑 같은게 있는지 확인
-			//근데 초깃값 설정 되어 있음
-			//EX. int a = 40;
-			param.setParamType(dev[0]);
-			String[] temp = dev[1].split("=");//=을 기준을 다시 자른다
-			
-			
-			param.setParamName(temp[0]);
-			param.setParamValue(temp[1].substring(0,dev[1].length()-1));
+		for(ParameterType parameterType : ParameterType.values()) {
+			System.out.println("for문 들어옴");
+			if(dev[0].toUpperCase().equals(parameterType.toString())&&!line.contains("=")) {
+				System.out.println("if문");
+				//enum 안에 있는 Strind과 split한 배열 첫번째랑 같은게 있는지 확인
+				//근데 초깃값 설정안되어 있는 경우
+				//EX. int a;
+				//toUpperCase() 이거는 소문자를 대문자로 바꾸는 함수
+				System.out.println(dev[0]+"이거 넣을게 초기화 안해도대는거야 ");
+				param.setParamType(dev[0]);
+				param.setParamName(dev[1].substring(0,dev[1].length()-1));
+				
+			} else if(dev[0].toUpperCase().equals(parameterType.toString())&&line.contains("=")) {
+				System.out.println("else if문");
+				//enum 안에 있는 String과 split한 배열 첫번째랑 같은게 있는지 확인
+				//근데 초깃값 설정 되어 있음
+				//EX. int a = 40;
+				param.setParamType(dev[0]);
+				System.out.println("setParamType============dev[0]에 넣음");
+				String[] temp = dev[1].split("=");//=을 기준을 다시 자른다
+				System.out.println("dev[1] "+dev[1]);
+				System.out.println("temp0  "+temp[0]);
+				System.out.println("temp1  "+temp[1]);
+				System.out.println("=기준 자름");
+				param.setParamName(temp[0]);
+				System.out.println("temp[0]에 넣음");
+				param.setParamLine(String.valueOf(lineNumber));
+				param.setParamValue(temp[1].substring(0,temp[1].length()-1));
+				
+				System.out.println("샷");
+			}
 		}
-		
 		return param;
+	}
+
+
+
+	@Override		//함수정의부분 만났을 경우
+	public MethodBean case_functionDefine(int lineNumber,String line,HashMap<String,String> splitcode) {
+		method = new MethodBean();
+		String[] dev = line.split(" ");
+		//EX. int method(int a,int b);
+		
+		
+		
+		
+		
+		
+		
+		return method;
 	}
 	
 	
+
+
 }
--- HelloWorld/src/main/java/com/owo/HelloWorld/Service/Service_Impl.java
+++ HelloWorld/src/main/java/com/owo/HelloWorld/Service/Service_Impl.java
@@ -8,6 +8,8 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Service;
 import com.owo.HelloWorld.Buffer.CodeBuffer;
 import com.owo.HelloWorld.Buffer.CoreBuffer;
+import com.owo.HelloWorld.Buffer.Bean.MethodBean;
+import com.owo.HelloWorld.Buffer.Bean.ParamBean;
 import com.owo.HelloWorld.Buffer.Enum.ParameterType;
 
 @Service
@@ -19,12 +21,13 @@ public class Service_Impl implements MainService{
 	 * line별로 스플릿한 해시맵을 라인수대로 차례차례검사하면서
 	 * corebuffer에 있는 해시맵에 다시 담아주는 작업을 실시한다.
 	 */
-	
+
 	@Inject
 	CodeBuffer codebuffer;
+
 	CoreBuffer corebuffer = new CoreBuffer();
-	MainService mainservice;
-	
+	MainService mainservice; 
+
 	@Override
 	public HashMap<String, String> splitCode(String text) {
 		HashMap<String, String> res = new HashMap<String, String>();
@@ -42,7 +45,7 @@ public class Service_Impl implements MainService{
 
 	@Override
 	public HashMap<String, String> methodSplit(HashMap<String, String> sc) { //메소드 잘라서 붙이는거임
-
+		//String mth = "void main(){ int a;, int b ; int c; c = a+b; printf("%d",c); }";
 		HashMap<String, String> res = new HashMap<String, String>();	//해쉬맵임
 		ArrayList list = new ArrayList();								//라인 나눈거 받은거 get한거 넣은 리스트임
 		String result; 										//결과값임
@@ -105,34 +108,64 @@ public class Service_Impl implements MainService{
 	}
 
 	@Override
-	public HashMap<String,String> lineRead(String code) {// 여기 string은 1라인씩 자른 코드의 한줄짜리 코드임
-		String[] lineSplit = code.split(" ");// 여기서 스플릿을 여러형태로 해줘야함
+	public Object lineRead(int lineNumber,String code,HashMap<String, String> splitcode) {// 여기 string은 1라인씩 자른 코드의 한줄짜리 코드임
+		//String[] lineSplit = code.split(" ");// 여기서 스플릿을 여러형태로 해줘야함
 		//만약 =이 있을 경우 양쪽의 값을 비교대조해줘야하거나
 		//케이스별로 짜개야함
 		//int a;
-		System.out.println("한라인만 읽기");
-		if(lineSplit[0].equals(ParameterType.INT)) {
 
+		System.out.println("한라인만 읽기");
+		System.out.println(code);
+
+		for(ParameterType parameterType : ParameterType.values()) {
+			if(code.toUpperCase().contains(parameterType.toString())&&!code.contains("(")) {			//변수선언일경우
+				System.out.println("int 드르가");
+				ParamBean parambean = new ParamBean();
+				parambean = codebuffer.case_VariableDeclaration(lineNumber,code);
+				System.out.println("paramname"+parambean.getParamName());
+				System.out.println("paramline"+parambean.getParamLine());
+				return parambean;
+			}
+			else if(code.toUpperCase().contains(parameterType.toString())&&code.contains("(")&&code.contains(")")) {										//함수선언일경우
+				System.out.println("함수케이스 들어가버렷스");
+				MethodBean methodbean = new MethodBean();
+				
+				methodbean = codebuffer.case_functionDefine(lineNumber,code,splitcode);
+			}
 		}
 
-		
+
 		return null;//임시
 
 	}
 
+
+
 	@Override
-	public CoreBuffer allRead(HashMap<String, String> hashmap) {
-		
+	public CoreBuffer allRead(HashMap<String, String> splitcode) {
+
+
+		CoreBuffer corebuffer = new CoreBuffer();
 		System.out.println("전부읽어들이기");
-		System.out.println(hashmap);
-		HashMap<String, String> temp = new HashMap<String,String>();
-		for(int i=0;i<hashmap.size();i++) {
-			temp = mainservice.lineRead(hashmap.get((i+1)+"line"));
-			//자기가 자기어떻게 부르더라
-			//여기서 터졌는데
+		System.out.println(splitcode);
+		//HashMap<String, String> temp = new HashMap<String,String>();
+
+
+		for(int i=1;i<splitcode.size()+1;i++) {
+			Object obj = new Object();
+			//	System.out.println(hashmap.get(i+"line"));
+			obj = lineRead(i,splitcode.get(i+"line"),splitcode);//리턴타입 오브젝트
+
+
+			System.out.println(obj);
+			if(obj instanceof ParamBean) {	//Object의 타입을 알아내기위함
+				System.out.println(obj instanceof ParamBean);
+
+				corebuffer.setParam(i+"line",obj);
+				System.out.println("같냐안간나");
+			}
 		}
-		
+
 		return corebuffer;
 	}
-
 }
